7f8434561d9dc38ec7648856d5f52639c0ca16a1
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-06-07 13:52:47 -0400

    Completed MVP and styling component stretch goal.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { TabStyle, SelectedTab } from "./StyledComponents";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
-      className={''}
+    <TabStyle
+      selected={props.tab === props.selectedTab ? true : false}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabStyle>
   );
 };
 
-// Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+};
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,24 +1,31 @@
 import React from "react";
 import Tab from "./Tab";
+import PropTypes from "prop-types";
+import { TabsStyle, Topics, TabsTitle } from "./StyledComponents";
+
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-        {props.tabs.map(tab => {
-          return (
-            <div>
-              {props.selectTabHandler}
-              {props.selectedTab}
-              {props.tab}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <TabsStyle>
+      <Topics>
+        <TabsTitle>TRENDING TOPICS:</TabsTitle>
+
+        {props.tabs.map((item, index) => (
+          <Tab
+            key={index}
+            tab={item}
+            selectTabHandler={props.selectTabHandler}
+            selectedTab={props.selectedTab}
+          />
+        ))}
+      </Topics>
+    </TabsStyle>
   );
+};
+
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
 };
 
 // Make sure to use PropTypes to validate your types!
